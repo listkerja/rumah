02decdc5d4ad51dd964c8ef147b5c21fa26dfa70
listkerja <132216346+listkerja@users.noreply.github.com>
2023-05-13 14:39:55 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://guuth.com` + encodeURI(path as string)
+					`https://guuth.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
